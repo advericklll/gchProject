7a3bda631b5cb45d872664042249b6dece6ae95d
Lucas <Lucas@192.168.6.134>
2014-04-13 16:50:11 -0400

    Creating Multiple Project

---
+++ gchHibernate/src/main/java/pe/edu/cibertec/gchhibernate/App.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.gchhibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
